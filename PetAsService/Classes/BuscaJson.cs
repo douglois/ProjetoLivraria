6e46493403eed52ebb362973216747b1579fe975
douglois <45090066+douglois@users.noreply.github.com>
2022-11-16 22:44:35 -0300

    Add files via upload

---
+++ PetAsService/Classes/BuscaJson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace PetAsService.Classes
+{
+    public class BuscaJson
+    {
+        public static string GetJson()
+        {
+            var request = WebRequest.Create("https://api.thecatapi.com/v1/breeds");
+            request.Method = "GET";
+            var response = (HttpWebResponse)request.GetResponse();
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                using(var stream = response.GetResponseStream())
+                {
+                    var reader = new StreamReader(stream);
+                    var json = reader.ReadToEnd();
+                    return json.ToString();
+                }
+            }
+            return null;
+        }
+    }
+}
